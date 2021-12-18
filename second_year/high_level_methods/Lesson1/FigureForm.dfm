612c3d0f891a894a56c0c802d681b86e8c8212b4
Danil Ovchinnikov <danil42worker@gmail.com>
2021-12-19 02:40:34 +0700

    high_level_methods: добавил 1 задание

---
+++ second_year/high_level_methods/Lesson1/FigureForm.dfm
@@ -0,0 +1,186 @@
+object Form1: TForm1
+  Left = 0
+  Top = 0
+  BorderIcons = [biSystemMenu]
+  BorderStyle = bsSingle
+  Caption = 'Figure'
+  ClientHeight = 509
+  ClientWidth = 731
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  Position = poDesigned
+  OnActivate = FormActivate
+  OnCreate = FormCreate
+  PixelsPerInch = 96
+  TextHeight = 13
+  object GroupBoxParams: TGroupBox
+    Left = 1
+    Top = 0
+    Width = 185
+    Height = 209
+    Caption = #1055#1072#1088#1072#1084#1077#1090#1088#1099' '#1079#1072#1075#1086#1090#1086#1074#1082#1080
+    TabOrder = 0
+    object LabelParamsOsnov: TLabel
+      Left = 16
+      Top = 130
+      Width = 66
+      Height = 13
+      Caption = #1044#1080#1072#1084#1077#1090#1088' ('#1089#1084')'
+    end
+    object LabelParamsHeight: TLabel
+      Left = 16
+      Top = 156
+      Width = 59
+      Height = 13
+      Caption = #1042#1099#1089#1086#1090#1072' ('#1089#1084')'
+    end
+    object LabelParamsMaterial: TLabel
+      Left = 16
+      Top = 184
+      Width = 50
+      Height = 13
+      Caption = #1052#1072#1090#1077#1088#1080#1072#1083
+    end
+    object DBRadioGroup1: TDBRadioGroup
+      Left = 24
+      Top = 24
+      Width = 158
+      Height = 96
+      Caption = #1042#1099#1073#1077#1088#1080#1090#1077' '#1086#1089#1085#1086#1074#1072#1085#1080#1077
+      Ctl3D = True
+      ParentCtl3D = False
+      TabOrder = 0
+    end
+    object RBtnCil: TRadioButton
+      Left = 32
+      Top = 41
+      Width = 113
+      Height = 17
+      Caption = #1050#1088#1091#1075
+      Checked = True
+      TabOrder = 1
+      TabStop = True
+      OnClick = EditZagotClick
+    end
+    object RBtnPar: TRadioButton
+      Left = 32
+      Top = 64
+      Width = 113
+      Height = 17
+      Caption = #1050#1074#1072#1076#1088#1072#1090
+      TabOrder = 2
+      OnClick = EditZagotClick
+    end
+    object EditParamsOsnov: TEdit
+      Left = 88
+      Top = 126
+      Width = 57
+      Height = 21
+      NumbersOnly = True
+      TabOrder = 3
+      Text = '0'
+    end
+    object EditParamsHeight: TEdit
+      Left = 88
+      Top = 153
+      Width = 57
+      Height = 21
+      NumbersOnly = True
+      TabOrder = 4
+      Text = '0'
+    end
+    object CBoxMaterial: TComboBox
+      Left = 88
+      Top = 180
+      Width = 57
+      Height = 21
+      TabOrder = 5
+      Items.Strings = (
+        'Cu'
+        'Al'
+        'Fi'
+        'Ne')
+    end
+    object RBtnPri: TRadioButton
+      Left = 32
+      Top = 87
+      Width = 113
+      Height = 17
+      Caption = #1055#1088#1080#1079#1084#1072
+      TabOrder = 6
+      OnClick = EditZagotClick
+    end
+  end
+  object GroupBoxDeform: TGroupBox
+    Left = 1
+    Top = 215
+    Width = 185
+    Height = 74
+    Caption = #1056#1072#1089#1095#1077#1090' '#1076#1077#1092#1086#1088#1084#1072#1094#1080#1080
+    TabOrder = 1
+    object LabelDeformOsnovData: TLabel
+      Left = 102
+      Top = 24
+      Width = 20
+      Height = 13
+      Caption = '0 '#1089#1084
+    end
+    object LabelDeformHeightData: TLabel
+      Left = 102
+      Top = 43
+      Width = 20
+      Height = 13
+      Caption = '0 '#1089#1084
+    end
+    object LabelDeformOsnov: TLabel
+      Left = 24
+      Top = 24
+      Width = 58
+      Height = 13
+      Caption = #1044#1080#1072#1084#1077#1090#1088' = '
+    end
+    object LabelDeformHeight: TLabel
+      Left = 24
+      Top = 43
+      Width = 51
+      Height = 13
+      Caption = #1042#1099#1089#1086#1090#1072' = '
+    end
+  end
+  object BtnCalk: TButton
+    Left = 24
+    Top = 301
+    Width = 137
+    Height = 25
+    Caption = #1056#1072#1089#1095#1077#1090
+    TabOrder = 2
+    OnClick = BtnCalkClick
+  end
+  object BtnExit: TButton
+    Left = 24
+    Top = 351
+    Width = 137
+    Height = 25
+    Caption = #1042#1099#1093#1086#1076
+    TabOrder = 3
+    OnClick = BtnExitClick
+  end
+  object GroupBoxVisualization: TGroupBox
+    Left = 192
+    Top = 0
+    Width = 531
+    Height = 501
+    Caption = #1042#1080#1076' '#1079#1072#1075#1086#1090#1086#1074#1082#1080
+    TabOrder = 4
+    object Image1: TImage
+      Left = 3
+      Top = 24
+      Width = 525
+      Height = 477
+    end
+  end
+end
---
+++ second_year/high_level_methods/Lesson1/FigureForm.pas
@@ -0,0 +1,319 @@
+﻿unit FigureForm;
+
+interface
+
+uses
+  Winapi.Windows,
+  Winapi.Messages,
+  System.SysUtils,
+  System.Variants,
+  System.Classes,
+  Vcl.Graphics,
+  Vcl.Controls,
+  Vcl.Forms,
+  Vcl.Dialogs,
+  Vcl.StdCtrls,
+  Vcl.ExtCtrls,
+  Vcl.DBCtrls;
+
+type
+  TForm1 = class(TForm)
+    GroupBoxParams: TGroupBox;
+    DBRadioGroup1: TDBRadioGroup;
+    RBtnCil: TRadioButton;
+    RBtnPar: TRadioButton;
+    RBtnPri: TRadioButton;
+    EditParamsOsnov: TEdit;
+    EditParamsHeight: TEdit;
+    CBoxMaterial: TComboBox;
+    GroupBoxDeform: TGroupBox;
+    LabelDeformOsnovData: TLabel;
+    LabelDeformHeightData: TLabel;
+    BtnCalk: TButton;
+    BtnExit: TButton;
+    GroupBoxVisualization: TGroupBox;
+    Image1: TImage;
+    LabelParamsOsnov: TLabel;
+    LabelParamsHeight: TLabel;
+    LabelParamsMaterial: TLabel;
+    LabelDeformOsnov: TLabel;
+    LabelDeformHeight: TLabel;
+    procedure FormCreate(Sender: TObject);
+    procedure BtnCalkClick(Sender: TObject);
+    procedure BtnExitClick(Sender: TObject);
+    procedure EditZagotClick(Sender: TObject);
+    procedure FormActivate(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+Type
+  TMaterial = (Cu, Al, Fe, Ni); // Материалы заготовок
+
+  TZagot = class // Абстрактная заготовка
+    Structure: TMaterial;
+    SHeight, DHeight, SOsnov, DOsnov: real;
+    Constructor Create;
+    Procedure Deform; virtual;
+    Procedure Show(Heig, Osn: real; Zc: TColor); virtual; abstract;
+    Procedure Draw;
+  End;
+
+  TCil = class(TZagot) // Цилиндр
+    constructor Сreate;
+    procedure Deform; override;
+    procedure Show(Heig, Osn: real; Zc: TColor); override;
+  end;
+
+  TPar = class(TZagot) // Параллелепипед
+    constructor Сreate;
+    procedure Deform; override;
+    procedure Show(Heig, Osn: real; Zc: TColor); override;
+  end;
+
+  TPri = class(TZagot) // Призма
+    constructor Create;
+    procedure Deform; override;
+    procedure Show(Heig, Osn: real; Zc: TColor); override;
+  end;
+
+var
+  m: real;
+
+{$REGION 'Абстрактная заготовка'}
+
+constructor TZagot.Create; // Создание абстрактной заготовки
+begin
+  inherited;
+  SHeight := 30;
+  DHeight := 1;
+  SOsnov := 10;
+  DOsnov := 1;
+  Structure := Cu;
+end;
+
+procedure TZagot.Deform; // Деформация абстрактной заготовки
+var
+  k: real;
+begin
+  k := 1;
+  case Structure of
+    Cu:
+      k := 2;
+    Al:
+      k := 3;
+    Fe:
+      k := 4;
+    Ni:
+      k := 5;
+  end;
+  DHeight := SHeight - SHeight / k;
+end;
+
+procedure TZagot.Draw; // Отрисовка заготовки
+begin
+  with Form1.Image1.Canvas do
+    Fillrect(ClipRect);
+  m := SHeight;
+  if SOsnov > m then
+    m := SOsnov;
+  if DHeight > m then
+    m := DHeight;
+  if DOsnov > m then
+    m := DOsnov;
+  m := 250 / m;
+
+  Show(m * DHeight, m * DOsnov, clRed); // После деформации
+  Show(m * SHeight, m * SOsnov, clBlack); // До деформации
+end;
+
+{$ENDREGION}
+{$REGION 'Цилиндр'}
+
+constructor TCil.Сreate; // Создание цилиндра
+begin
+  inherited;
+end;
+
+procedure TCil.Deform; // Деформация цилиндр
+begin
+  inherited;
+  DOsnov := 2 * Sqrt(SHeight * Sqr(SOsnov * 0.5) / DHeight);
+end;
+
+procedure TCil.Show(Heig: real; Osn: real; Zc: TColor); // Отрисовка цилиндра
+begin
+  with Form1.Image1.Canvas do
+  begin
+    Pen.color := Zc;
+    Pen.Style := psSolid;
+
+    // Рисуем верхние основание
+    Ellipse(160 - Round(Osn * 0.5), 10 + Round(m * SHeight - Heig), 160 + Round(Osn * 0.5),
+      10 + Round(m * SHeight - Heig + Osn * 0.3));
+
+    // Рисуем нижние основание
+    Arc(160 - Round(Osn * 0.5), 10 + Round(m * SHeight), 160 + Round(Osn * 0.5),
+      10 + Round(m * SHeight + Osn * 0.3), 160 - Round(Osn * 0.5),
+      10 + Round(m * SHeight + Osn * 0.15), 160 + Round(Osn * 0.5),
+      10 + Round(m * SHeight + Osn * 0.15));
+
+    // Рисуем боковые грани
+    MoveTo(160 - Round(Osn * 0.5), 10 + Round(m * SHeight - Heig + Osn * 0.15));
+    LineTo(160 - Round(Osn * 0.5), 10 + Round(m * SHeight + Osn * 0.15));
+    MoveTo(160 + Round(Osn * 0.5), 10 + Round(m * SHeight - Heig + Osn * 0.15));
+    LineTo(160 + Round(Osn * 0.5), 10 + Round(m * SHeight + Osn * 0.15));
+  end;
+end;
+
+{$ENDREGION}
+{$REGION 'Параллелепипед'}
+
+constructor TPar.Сreate; // Создание параллелепипеда
+begin
+  inherited;
+end;
+
+procedure TPar.Deform; // Деформация параллелепипеда
+begin
+  inherited;
+  DOsnov := Sqrt(SHeight * Sqr(SOsnov) / DHeight);
+end;
+
+procedure TPar.Show(Heig: real; Osn: real; Zc: TColor); // Отрисовка параллелепипеда
+begin
+  with Form1.Image1.Canvas do
+  begin
+    Pen.color := Zc;
+    Pen.Style := psSolid;
+
+    // Рисуем верхние основание
+    Polygon([point(220 - Round(Osn * 0.5), 10 + Round(m * SHeight - Heig)),
+      point(220 + Round(Osn * 0.5), 10 + Round(m * SHeight - Heig)), point(220 + Round(Osn * 0.2),
+      10 + Round(m * SHeight - Heig + Osn * 0.3)), point(220 - Round(Osn * 0.8),
+      10 + Round(m * SHeight - Heig + Osn * 0.3))]);
+
+    // Рисуем нижние основание и боковые грани
+    MoveTo(220 - Round(Osn * 0.8), 10 + Round(m * SHeight - Heig + Osn * 0.3));
+    LineTo(220 - Round(Osn * 0.8), 10 + Round(m * SHeight + Osn * 0.3));
+    LineTo(220 + Round(Osn * 0.2), 10 + Round(m * SHeight + Osn * 0.3));
+    LineTo(220 + Round(Osn * 0.5), 10 + Round(m * SHeight));
+    LineTo(220 + Round(Osn * 0.5), 10 + Round(m * SHeight - Heig));
+    MoveTo(220 + Round(Osn * 0.2), 10 + Round(m * SHeight - Heig + Osn * 0.3));
+    LineTo(220 + Round(Osn * 0.2), 10 + Round(m * SHeight + Osn * 0.3));
+  end;
+end;
+
+{$ENDREGION}
+{$REGION 'Призма'}
+
+constructor TPri.Create; // Создание призмы
+begin
+  inherited;
+end;
+
+procedure TPri.Deform; // Деформация призмы
+begin
+  inherited;
+  DOsnov := Sqrt(SHeight * Sqr(SOsnov) / DHeight);
+end;
+
+procedure TPri.Show(Heig: real; Osn: real; Zc: TColor); // Отрисовка призмы
+begin
+  with Form1.Image1.Canvas do
+  begin
+    Pen.color := Zc;
+    Pen.Style := psSolid;
+
+    // Рисуем верхние основание
+    Polygon([point(220 + Round(Osn * 0.5), 10 + Round(m * SHeight - Heig)),
+      point(220 - Round(Osn * 0.2), 10 + Round(m * SHeight - Heig + Osn * 0.3)),
+      point(220 + Round(Osn * 0.8), 10 + Round(m * SHeight - Heig + Osn * 0.3))]);
+
+    // Рисуем нижние основание и боковые грани
+    MoveTo(220 - Round(Osn * 0.2), 10 + Round(m * SHeight - Heig + Osn * 0.3));
+    LineTo(220 - Round(Osn * 0.2), 10 + Round(m * SHeight + Osn * 0.3));
+    LineTo(220 + Round(Osn * 0.8), 10 + Round(m * SHeight + Osn * 0.3));
+    LineTo(220 + Round(Osn * 0.8), 10 + Round(m * SHeight - Heig + Osn * 0.3));
+
+    // Рисуем внутренние грани
+    Pen.Style := psDot;
+
+    MoveTo(220 + Round(Osn * 0.5), 10 + Round(m * SHeight + Osn * 0.005));
+    LineTo(220 + Round(Osn * 0.5), 10 + Round(m * SHeight - Heig + Osn * 0.005));
+
+    MoveTo(220 + Round(Osn * 0.8), 10 + Round(m * SHeight + Osn * 0.3));
+    LineTo(220 + Round(Osn * 0.5), 10 + Round(m * SHeight + Osn * 0.005));
+
+    MoveTo(220 - Round(Osn * 0.2), 10 + Round(m * SHeight + Osn * 0.3));
+    LineTo(220 + Round(Osn * 0.5), 10 + Round(m * SHeight + Osn * 0.005));
+
+  end;
+end;
+
+{$ENDREGION}
+
+procedure TForm1.BtnCalkClick(Sender: TObject); // Процедура расчета
+begin
+  var
+    MyZagot: TZagot;
+  begin
+    if RBtnCil.Checked then
+      MyZagot := TCil.Create
+    else if RBtnPar.Checked then
+      MyZagot := TPar.Create
+    else
+      MyZagot := TPri.Create;
+
+    with MyZagot do
+    begin
+      SOsnov := StrToFloat(EditParamsOsnov.Text);
+      SHeight := StrToFloat(EditParamsHeight.Text);
+      Structure := Cu;
+      inc(Structure, CBoxMaterial.ItemIndex);
+      Deform;
+      Draw;
+      LabelDeformOsnovData.Caption := FloatToStrF(DOsnov, ffFixed, 10, 2) + ' см';
+      LabelDeformHeightData.Caption := FloatToStrF(DHeight, ffFixed, 10, 2) + ' см';
+    end;
+  end;
+end;
+
+procedure TForm1.BtnExitClick(Sender: TObject); // Процедура закрытия
+begin
+  close();
+end;
+
+procedure TForm1.FormActivate(Sender: TObject);
+begin
+  Form1.Image1.Canvas.Rectangle(0, 0, Image1.Width, Image1.Height);
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  CBoxMaterial.ItemIndex := 0;
+end;
+
+procedure TForm1.EditZagotClick(Sender: TObject);
+var
+  zn: string;
+begin
+  if RBtnCil.Checked then
+    zn := 'Диаметр'
+  else
+    zn := 'Сторона';
+
+  LabelParamsOsnov.Caption := zn + ' (см)';
+  LabelDeformOsnov.Caption := zn + ' =';
+end;
+
+end.
