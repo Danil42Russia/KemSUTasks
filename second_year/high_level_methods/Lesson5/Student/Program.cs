aa4967355ebda7fc45ffeca3974def32877dba8a
Danil Ovchinnikov <danil42worker@gmail.com>
2021-12-21 22:25:49 +0700

    high_level_methods: добавил 5 задание

---
+++ second_year/high_level_methods/Lesson5/Student/Program.cs
@@ -0,0 +1,105 @@
+﻿namespace Student;
+
+static class Program
+{
+    private const int MaxCountStudents = 10;
+    private static readonly List<Student> Students = new(MaxCountStudents);
+
+    private static void Main()
+    {
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("<== Меню ==>");
+            Console.WriteLine("A. Добавить студента");
+            Console.WriteLine("S. Показать студентов");
+            Console.WriteLine("Q. Выход");
+
+            var position = Console.ReadLine() ?? "";
+            switch (position)
+            {
+                case "Q":
+                    return;
+                case "A":
+                    AddStudent();
+                    break;
+                case "S":
+                    ShowStudents();
+                    break;
+            }
+        }
+    }
+
+    private static Student CreateStudent()
+    {
+        Console.Write("Введите фамилию и инициалы студента: ");
+        var name = Console.ReadLine();
+        if (string.IsNullOrEmpty(name))
+            throw new ArgumentException("Имя студента не может быть пустым");
+
+        Console.Write("Введите номер группы студента: ");
+        var group = Console.ReadLine();
+        if (string.IsNullOrEmpty(group))
+            throw new ArgumentException("Номер группы не может быть пустым");
+
+        Console.Write("Введите оценки студента (через зяпятую или пробел): ");
+        var evaluation = Console.ReadLine();
+        var evaluationArray = string.IsNullOrEmpty(evaluation)
+            ? Array.Empty<string>()
+            : evaluation.Trim().Split(',', ' ');
+
+        var evaluations = Array.ConvertAll(evaluationArray, Convert.ToInt32);
+
+        return new Student
+        {
+            Surname = name,
+            GroupNumber = group,
+            Evaluations = evaluations
+        };
+    }
+
+    private static void AddStudent()
+    {
+        Console.Clear();
+        Console.WriteLine("<== Добавить студента ==>");
+
+        if (Students.Count == MaxCountStudents)
+        {
+            Console.WriteLine($"Израсходовано {MaxCountStudents} мест на добавалие студентов");
+            Console.ReadLine();
+            return;
+        }
+
+        try
+        {
+            var student = CreateStudent();
+            Students.Add(student);
+
+            Console.WriteLine($"Студент '{student.ToString()}' успешно добавлен!");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Ошибка при создание студента: {e.Message}");
+        }
+
+        Console.ReadLine();
+    }
+
+    private static void ShowStudents()
+    {
+        Console.Clear();
+        Console.WriteLine("<== Показать студентов ==>");
+
+        var students = Students
+            .Where(student => student.Average > 4.0)
+            .ToList();
+
+        if (students.Count == 0)
+            Console.WriteLine("Студентов с средним баллом больше 4.0 не обнаружено!");
+
+        foreach (var student in students)
+            Console.WriteLine($"Студент '{student.ToString()}'");
+
+        Console.ReadLine();
+    }
+}
---
+++ second_year/high_level_methods/Lesson5/Student/Student.cs
@@ -0,0 +1,37 @@
+﻿namespace Student;
+
+public readonly struct Student
+{
+    private const uint MaxEvaluationsCount = 5;
+
+    private readonly int[] _evaluations;
+
+    public string Surname { init; get; }
+    public string GroupNumber { init; get; }
+    public double Average { private init; get; }
+
+    public int[] Evaluations
+    {
+        init
+        {
+            if (value.Length > MaxEvaluationsCount)
+                throw new ArgumentException($"Максимальное количество оценок: {MaxEvaluationsCount}");
+
+            var invalid = value
+                .Where(v => v > 5 || v < 1)
+                .ToList();
+            if (invalid.Count != 0)
+                throw new ArgumentException($"Недопустимое значение оценки: {invalid[0]}");
+
+            Average = value.Average();
+
+            _evaluations = value;
+        }
+        get => _evaluations;
+    }
+
+    public override string ToString()
+    {
+        return $"фамилия: {Surname} группа: {GroupNumber} средний балл: {Average} ({string.Join(", ", Evaluations)})";
+    }
+}
